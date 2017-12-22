142585c0a3b48c2d069a36e58d6a8a165fc3ecc8
Igor Knop <igorknop@gmail.com>
2017-12-22 15:39:31 -0200

    Add click handler

--- js/game.js
+++ js/game.js
@@ -1,40 +1,59 @@
 export {Game}
+
 class Game {
-
-  constructor(canvas) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
-    this.width = canvas.width;
-    this.height = canvas.height;
-    this.setupControls();
+  constructor (canvas) {
+    this.canvas = canvas
+    this.ctx = canvas.getContext('2d')
+    this.width = canvas.width
+    this.height = canvas.height
+    this.setupControls()
   }
 
-  clearScreen(){
-    this.ctx.fillStyle = 'black';
-    this.ctx.fillRect(0,0, this.width, this.height);
+  clearScreen () {
+    this.ctx.fillStyle = 'black'
+    this.ctx.fillRect(0, 0, this.width, this.height)
   }
 
-  setupControls(){
-    addEventListener('keydown', function(e){
-      console.log(e.keyCode);
-    });
-    addEventListener('keyup', function(e){
-      console.log(e.keyCode);
-    });
-    addEventListener('click', function(e){
-      console.log('click');
-      var element = this.canvas;
-      var offsetX = 0, offsetY = 0
+  setupControls () {
+    var that = this
+    window.addEventListener('keydown', function (e) {
+      console.log(e.keyCode)
+    })
+    window.addEventListener('keyup', function (e) {
+      console.log(e.keyCode)
+    })
+    window.addEventListener('click', function (e) {
+      var element = that.canvas
+      var offsetX = 0
+      var offsetY = 0
 
       if (element.offsetParent) {
         do {
-          offsetX += element.offsetLeft;
-          offsetY += element.offsetTop;
-        } while ((element = element.offsetParent));
+          offsetX += element.offsetLeft
+          offsetY += element.offsetTop
+        } while ((element = element.offsetParent))
       }
-      x = e.pageX - offsetX;
-      y = e.pageY - offsetY;
-      console.log(x,y);
-    }, false);
+      var x = e.pageX - offsetX
+      var y = e.pageY - offsetY
+      console.log('click', e.pageX, e.pageY)
+      console.log('offset', x, y)
+      that.handleClick(x, y)
+    }, false)
+  }
+  handleClick (x, y) {
+    this.ctx.strokeStyle = 'white'
+    this.ctx.lineWidth = 1
+    this.ctx.beginPath()
+    this.ctx.moveTo(x, y - 10)
+    this.ctx.lineTo(x, y + 10)
+    this.ctx.moveTo(x - 10, y)
+    this.ctx.lineTo(x + 10, y)
+    this.ctx.closePath()
+    this.ctx.stroke()
+    this.ctx.beginPath()
+    this.ctx.fillStyle = 'white'
+    this.ctx.rect(x - 5, y - 5, 10, 10)
+    this.ctx.closePath()
+    this.ctx.stroke()
   }
 }
